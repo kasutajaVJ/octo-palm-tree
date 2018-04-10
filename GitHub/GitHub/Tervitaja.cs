e466510ba4eeba1e7e1aa319de3b5335b3b9df84
kasutajaVJ <vanessa.jalakas@gmail.com>
2018-04-10 18:53:39 +0300

    Create Tervitaja class with Tervita method

---
+++ GitHub/GitHub/Tervitaja.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitHub
+{
+    internal class Tervitaja
+    {
+        /// <summary>
+        /// Show greeting to console.
+        /// </summary>
+        /// <param name="nimi"></param>
+        internal static void Tervita(string nimi)
+        {
+           Console.WriteLine("Hello " + nimi);
+        }
+    }
+}
